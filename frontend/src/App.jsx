3fdb6e27d836b3a65a97c537f3f04fe4ded2d75f
Majid0899 <majidkhan0899@gmail.com>
2025-08-27 14:59:21 +0530

    install frontend dependencies set up router and redux store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Layout from './components/Layout'
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div>App</div>
+    <div className='no-scrollbar'>
+    <Layout />
+    <ToastContainer position="top-right" autoClose={3000} />
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,51 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+import Home from './pages/Home.jsx';
+import Doctors from './pages/Doctors.jsx';
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import SignUp from './pages/SignUp.jsx';
+
+
+const router=createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/doctors",
+        element: <Doctors />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path:"/signup",
+        element:<SignUp />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+    
   </StrictMode>,
 )
