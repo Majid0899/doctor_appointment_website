d03bfccab12c0f3f8589ff9f4c8a634587ea7857
Majid0899 <majidkhan0899@gmail.com>
2025-08-27 14:56:15 +0530

    change the  default image of doctor and user

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -5,7 +5,7 @@ const doctorSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, select: false }, 
-    image: { type: String, default:"https://i.pravatar.cc/150?img=1"}, 
+    image: { type: String, default:"https://sdmntprwestus2.oaiusercontent.com/files/00000000-201c-61f8-944b-03a38efa388f/raw?se=2025-08-26T17%3A48%3A50Z&sp=r&sv=2024-08-04&sr=b&scid=d0864edd-d786-5297-909b-d23f341c1622&skoid=03727f49-62d3-42ac-8350-1c0e6559d238&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-08-26T13%3A13%3A47Z&ske=2025-08-27T13%3A13%3A47Z&sks=b&skv=2024-08-04&sig=lR0n2AVqQHwxEcvlz9sHO2HvvuJda4kcN/ogAtXuVjE%3D"}, 
     speciality: { type: String, required: true },
     degree: { type: String, required: true },
     experience: { type: Number, required: true, min: 0 },
--- backend/models/User.js
+++ backend/models/User.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
   },
   image: {
     type: String,
-    default: "https://i.pravatar.cc/150?img=1",
+    default: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-c9c8-61f6-bc1d-5dcd792b8f08/raw?se=2025-08-26T17%3A47%3A34Z&sp=r&sv=2024-08-04&sr=b&scid=1e942c6f-0035-5a89-963a-0060d43a23e2&skoid=03727f49-62d3-42ac-8350-1c0e6559d238&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-08-26T02%3A42%3A24Z&ske=2025-08-27T02%3A42%3A24Z&sks=b&skv=2024-08-04&sig=QxodhPZxn1Kw6xrWUYcKXn32PGGW7INZbC7PSOgb1N4%3D",
   },
   gender: {
     type: String,
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema({
   },
   dateOfBirth: {
     type: Date,
-    default: null,
+    required:true
   },
 }, { timestamps: true });
 
